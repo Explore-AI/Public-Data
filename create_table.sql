9665d803f303d9bf14816d76e3bbec6657ac4ce9
Nthiki <75297833+Nthiki@users.noreply.github.com>
2021-06-23 16:33:08 +0200

    Add files via upload

---
+++ create_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE public.book_data(
+book_id VARCHAR(56) NULL,
+author VARCHAR(56) NULL,
+title VARCHAR(56) NULL,
+genre VARCHAR(56) NULL,
+price FLOAT NULL,
+publish_date VARCHAR(56) NULL,
+description VARCHAR(256) NULL
+);
