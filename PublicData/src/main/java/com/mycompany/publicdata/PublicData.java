b0115d7a1c0893db5cf9d5a4619db584d5ad7079
hoang <hoang@Ken>
2024-09-28 05:48:30 -0500

    Initial commit

---
+++ PublicData/src/main/java/com/mycompany/publicdata/PublicData.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.publicdata;
+
+/**
+ *
+ * @author hoang
+ */
+public class PublicData {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
